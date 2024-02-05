e3da43f8febcf82e054f340786e16b7ad4b35ebe
kabiljaziri <jazirikabil7@gmail.com>
2024-02-05 21:30:21 +0100

    button

---
+++ Web Fundamentals/week_1/Day_5/Practice/Loop.js
@@ -0,0 +1,20 @@
+for (var i=1;1<=20;i++){
+    console.log(i);
+}
+
+
+
+
+for (var i=1;i<=100;i++){
+if (i%3===0){
+    console.log(i);
+}
+}
+
+
+for( var i=4;i>=-3.5;i-=1.5){
+    if (i-=1.5){
+
+    }
+        console.log(i);
+    }
---
+++ Web Fundamentals/week_2/Day_1/Practice/button clicker/script.js
@@ -0,0 +1,9 @@
+function like(){
+    alert("Ninja was liked")
+}
+function remove(click){
+    click.remove()
+}
+function change(value){
+    value.innerHTML="Log out"
+}
