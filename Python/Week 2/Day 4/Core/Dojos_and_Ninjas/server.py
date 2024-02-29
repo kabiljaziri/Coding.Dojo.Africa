9c1007a4cf861a77fcc889d9435d5abea2bdd4e1
kabiljaziri <jazirikabil7@gmail.com>
2024-02-29 22:29:06 +0100

    dojos

---
+++ Python/Week 2/Day 4/Core/Dojos_and_Ninjas/server.py
@@ -0,0 +1,13 @@
+from flask_app import app
+
+# ! All controllers must be imported Here
+from flask_app.controllers import dojos_controller
+from flask_app.controllers import ninjas_controller
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug = True, port = 5001)
